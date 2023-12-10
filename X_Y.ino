548a5f9af6c6870759f1a6957a28d9c490993bc9
naengku3 <127822478+naengku3@users.noreply.github.com>
2023-12-11 00:22:03 +0900

    Update X_Y.ino

--- X_Y.ino
+++ X_Y.ino
@@ -7,17 +7,13 @@ int b = 0;
 
 void loop() {
   if (Serial.available() > 0) {
-    String a = Serial.readString();
-    a.trim();
-     // trim을 사용하기 전에 문자열 값을 정리합니다.
-    if (a.length() > 0 && b == 0) {
-      double value = a.toDouble();
-      if (value >= 50) {
-        Serial.println(value);
+    double a = atof(Serial.readStringUntil('\n').c_str());
+      if (a >= 50 && b == 0) {
+        Serial.println(a);
         tone(4, 523, 500);
         delay(500);
         b = 1;
       }
     }
   }
-}
+
